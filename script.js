5766145cd8378b162e858223a9fefd392123c8e0
andretiago23 <tiagoclossfilho@gmail.com>
2025-11-17 11:39:34 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+idade = "16"
+dataAniversario = '23 de setembro'
+console.log('minha data de aniiversario: ', dataAniversario)
+console.log('minha idade atual: ', idade)
+idade = "17"
+
+console.log('minha idade depois do aniversario: ', idade)
