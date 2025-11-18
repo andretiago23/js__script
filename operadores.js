365b6af1414de0f69c69e84563cf522fcccda9c6
andretiago23 <tiagoclossfilho@gmail.com>
2025-11-18 11:28:55 -0400

    matematica

---
+++ operadores.js
@@ -0,0 +1,15 @@
+nume1 = 10
+nume2 = 5
+subtracao = nume1 - nume2
+console.log('subtracao', subtracao)
+adicao = nume1 + nume2
+console.log('adicao', adicao)
+multiplicacao = nume1 * nume2
+console.log('multiplicação', multiplicacao)
+divisao = nume1 / nume2
+console.log('divisao', divisao)
+
+
+
+
+
--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-idade = "16"
-dataAniversario = '23 de setembro'
-console.log('minha data de aniiversario: ', dataAniversario)
-console.log('minha idade atual: ', idade)
-idade = "17"
-
-console.log('minha idade depois do aniversario: ', idade)
